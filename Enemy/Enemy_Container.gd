688744acb4e07ac08736c183f5c28d724f959f4a
mahanen15 <mahanen@iu.edu>
2022-10-31 18:57:42 -0400

    COMPLETE

--- Enemy/Enemy_Container.gd
+++ Enemy/Enemy_Container.gd
@@ -1,9 +1,14 @@
 extends Node
 
 onready var Bat = load("res://Enemy/Bat.tscn")
+onready var Knight = load("res://Enemy/Knight.tscn")
 
 func _physics_process(_delta):
 	if not has_node("Bat"):
 		var bat = Bat.instance()
 		add_child(bat)
 		bat.name = 'Bat'
+		if not has_node("Knight"):
+			var knight = Knight.instance()
+			add_child(knight)
+			bat.name = 'Knight'
